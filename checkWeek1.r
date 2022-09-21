4d7f40e8ad9629bedfd4e5c71dbfb7dbf5479295
brian <brian.holt@seattlecolleges.edu>
2022-09-21 09:40:39 -0700

    end of quarter commit

---
+++ checkWeek1.r
@@ -0,0 +1,25 @@
+library(rvest)
+library(tidyverse)
+library(clipr)
+library(lubridate)
+
+users<- read.csv("slackUsers.csv")
+roster <- read.csv("5-16-22_abnormal_roster.csv")
+roster %>% separate(Name,into = c('last','first'),sep = ',')
+
+week1 <- read_clip()
+week1
+
+posted<- grep("[0-9]{1,2}:|joined",week1,value = T)  %>%
+  as.data.frame(row.names = NULL) %>%
+  separate (col=".",into=c("name","time"),sep = "  ") %>%
+  mutate(check=lead(name)) %>%
+  mutate(check=ifelse(grepl("joined",check,ignore.case=T),1,0)) %>%
+  filter(!grepl('joined',name,ignore.case=T) ) %>%
+  filter (check!=1) %>%
+  select(name) %>%  unique(.)
+
+
+
+users %>% anti_join(posted, by = c("displayname"="name")) %>% filter(status =="Member") %>% select(displayname,email,fullname) %>%
+  mutate(message= paste0("Hi ",displayname,", Week 2 is getting close to being half over, and I'm not seeing a post to week 1. I might be wrong because I'm using program to filter for people who haven't posted yet, but I don't think you've posted to week 1 yet.  Do you need any help?  The #logistics channel is available."))
